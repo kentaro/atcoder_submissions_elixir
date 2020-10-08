a63e453eea7f8e00470041f03335d0b9c0631921
Kentaro Kuribayashi <kentarok@gmail.com>
2020-10-09 00:22:42 +0900

    add arc104 b

---
+++ arc104/B.cxx
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+    int ans = 0;
+
+    for (int i = 0; i < N; ++i) {
+        int c1 = 0;
+        int c2 = 0;
+        for (int j = i; j < N; ++j) {
+            if (S[j] == 'A') {
+                ++c1;
+            } else if (S[j] == 'T') {
+                --c1;
+            } else if (S[j] == 'C') {
+                ++c2;
+            } else {
+                --c2;
+            }
+
+            if (c1 == 0 && c2 == 0) {
+                ++ans;
+            }
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
--- arc104/B.rb
+++
@@ -1,6 +0,0 @@
-# https://atcoder.jp/contests/arc104/tasks/arc104_b
-
-
-N, S = gets.chomp.split
-
-puts ans
