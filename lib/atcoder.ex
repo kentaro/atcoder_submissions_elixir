cc10f9687eecaa17eed36c73aed7c96df518f397
Kentaro Kuribayashi <kentarok@gmail.com>
2020-12-16 01:40:39 +0900

    mix new atcoder

---
+++ lib/atcoder.ex
@@ -0,0 +1,18 @@
+defmodule Atcoder do
+  @moduledoc """
+  Documentation for `Atcoder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Atcoder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/atcoder_test.exs
@@ -0,0 +1,8 @@
+defmodule AtcoderTest do
+  use ExUnit.Case
+  doctest Atcoder
+
+  test "greets the world" do
+    assert Atcoder.hello() == :world
+  end
+end
