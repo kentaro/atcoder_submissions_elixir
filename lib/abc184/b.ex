bd9860c05dde0bac683d0268164e3157494adfc6
Kentaro Kuribayashi <kentarok@gmail.com>
2021-01-07 23:07:23 +0900

    add abc184 b

--- lib/abc184/b.ex
+++ lib/abc184/b.ex
@@ -1,29 +1,26 @@
 defmodule Abc184.B.Main do
-  def read_single() do
-    IO.read(:line) |> String.trim() |> String.to_integer()
-  end
-
-  def read_list() do
-    IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
-  end
-
-  def read_nested_list_for(length) do
-    1..length
-      |> Enum.reduce([], fn _, acc ->
-        list =
-          IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
-        [list | acc]
-      end)
-      |> Enum.reverse()
-  end
-
   def main() do
+    [_, x] = read_list()
     read_single()
-    |> solve()
+    |> solve(x)
     |> IO.puts()
   end
 
-  def solve(n) do
-    n
+  defp solve(s, x) do
+    String.split(s, "", trim: true)
+    |> Enum.reduce(x, fn c, acc ->
+      case c do
+        "o" -> acc + 1
+        "x" -> Enum.max([0, (acc - 1)])
+      end
+    end)
+  end
+
+  defp read_single() do
+    IO.read(:line) |> String.trim()
+  end
+
+  defp read_list() do
+    IO.read(:line) |> String.trim() |> String.split(" ") |> Enum.map(&String.to_integer/1)
   end
 end
